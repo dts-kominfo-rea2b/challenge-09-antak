0cdd611d4cc218441795076b1d94ba2c79b9789b
antak <ananta.tw@gmail.com>
2022-06-19 19:16:18 +0700

    Challenge done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from './data/contacts.json';
+import Contact from './components/Contact';
+import Header from './components/Header';
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+            <Header />
+      <div id='contact-list'>
+        { contacts.map(contact => <Contact key={contact.email} data={contact} />) }
+      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import '../App.css';
 
 const Header = () => {
     return (
-        <></>
+        <div>
+            <div className='Header'>
+                <h2>Call a Friend</h2>
+                <p>Your friendly contact app</p>
+            </div>
+        </div>
     )
 }
 
